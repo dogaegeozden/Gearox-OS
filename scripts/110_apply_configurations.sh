18915ef1d829dd7d57d2744bd1ed2393e7e8ef52
= <=>
2023-03-01 05:39:15 -0500

    Adding swap area and ssh configurations

--- scripts/110_apply_configurations.sh
+++ scripts/110_apply_configurations.sh
@@ -22,6 +22,14 @@ declare_variables() {
 	list_of_user_alias_strings=("# Electro Linux" "alias changeip='systemctl restart tor.service'" "alias torstop='systemctl stop tor.service'" "alias torstart='systemctl start tor.service'" "alias torstatus='systemctl status tor.service'")
 	# Creating a list called list_of_root_alias_strings
 	list_of_root_alias_strings=("# Electro Linux" "alias changeip='systemctl restart tor.service'" "alias myip='proxychains curl ifconfig.io'" "alias torstop='systemctl stop tor.service'" "alias torstart='systemctl start tor.service'" "alias torstatus='systemctl status tor.service'")
+	# Creating a variable called amount_of_free_swap_space_data
+	amount_of_free_swap_space_data=`free -m | grep Swap:`
+	# Creating a path which leads to the fstab file. Hint: fstab file is the file where you define the file systems that will be mount automatically.
+	fstab_file_path="/etc/fstab"
+	# Creating a path which leads to the location where fail2ban ssh local file will be created.
+	fail2ban_ssh_local_file_path="/etc/fail2ban/jail.d/sshd.conf"
+	# Creating a string which includes the fail2ban ssh local file's content
+	ssh_local_file_content="[sshd]\nenabled = true\nport = ssh\nfilter = sshd\nlogpath = /var/log/auth.log\nmaxretry = 3\nbantime = 120\nignoreip = whitelist-IP" 
 }
 
 main() {
@@ -37,10 +45,14 @@ main() {
 	create_the_user_aliases
 	# Calling the create_the_root_aliases function.
 	create_the_root_aliases
+	# Calling the configure_ssh function.
+	configure_ssh
 	# Calling the set_up_the_fail2ban function.
 	set_up_the_fail2ban
 	# Calling the set_up_the_vnstat function.
 	set_up_the_vnstat
+	# Calling the enable_swap_file function.
+	enable_swap_file
 }
 
 
@@ -55,19 +67,11 @@ configure_the_proxychains() {
 	#  A function which configures the proxychains software.
 
 	# Replace the search variable with replace variable in the idenfied file
-	if [[ $search0 != "" && $replace0 != "" ]]; then
-		sed -i "s/$search0/$replace0/" $proxychain_conf_file
-	fi
-
+	sed -i "s/$search0/$replace0/" $proxychain_conf_file
 	# Replace the search variable with replace variable in the idenfied file
-	if [[ $search1 != "" && $replace1 != "" ]]; then
-		sed -i "s/$search1/$replace1/" $proxychain_conf_file
-	fi
-
+	sed -i "s/$search1/$replace1/" $proxychain_conf_file
 	# Replace the search variable with replace variable in the idenfied file
-	if [[ $search2 != "" && $replace2 != "" ]]; then
-		sed -i "s/$search2/$replace2/" $proxychain_conf_file
-	fi
+	sed -i "s/$search2/$replace2/" $proxychain_conf_file
 
 	# Restarting the tor service to apply the changes
 	systemctl restart tor.service
@@ -103,6 +107,18 @@ create_the_root_aliases() {
 	source /root/.bashrc
 }
 
+configure_ssh() {
+	# A function which configures the ssh 
+
+	if [[ `grep "PermitRootLogin yes" "/etc/ssh/sshd_config"` ]]; then 
+		sed -i "s/PermitRootLogin yes/PermitRootLogin no/" "/etc/ssh/sshd_config"
+	fi
+
+	if [[ `grep "PasswordAuthentication yes" "/etc/ssh/sshd_config"` ]]; then 
+		sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/" "/etc/ssh/sshd_config"
+	fi
+}
+
 set_up_the_fail2ban() {
 	# A function which sets up the fail2ban software.
 
@@ -113,6 +129,10 @@ set_up_the_fail2ban() {
 	else
 		echo "fail2ban is alread instaled in your system."
 	fi
+
+	# Creating a local file to configure fail2ban for ssh
+	echo -e $ssh_local_file_content > $fail2ban_ssh_local_file_path
+
 	# Checking if the service is dead
 	if [[ `systemctl status fail2ban` == *"inactive (dead)"* ]]; then
 		# Starting the service
@@ -140,6 +160,47 @@ set_up_the_vnstat() {
 	vnstat -i $wifi_adaptor_name
 	# Initializing the database
 	vnstat
+}
+
+enable_swap_file () {
+	# A function which enables the swap area. Hint: Swap area is a type of file system which holds data when RAM is used up. It's preventing app crashes
+
+	# Checking if myswap file is not exists
+	if [[ ! -f "/var/opt/myswap" ]]; then
+		# Creating a swap file
+		dd if="/dev/zero" of="/var/opt/myswap" bs=1M count=16384
+		# Setting up the linux swap area
+		mkswap "/var/opt/myswap"
+		# Enabling the swap 
+		swapon "/var/opt/myswap"
+		if [[ ! `grep "/var/opt/myswap swap swap defaults 0 0" $fstab_file_path` ]]; then
+			# Make the swap area permanent
+			echo "/var/opt/myswap swap swap defaults 0 0" >> $fstab_file_path
+		fi
+
+	# Checking if myswap file is exists
+	else
+		# Creating an array called strarr by splitting the command's output with IFS delimiter.
+		IFS=' ' read -a strarr <<< "$amount_of_free_swap_space_data"
+		# Identifying the total swap size
+		swap_size="${strarr[1]}"
+		# Checking if total swap size is smaller than  10000 mega bytes 
+		if [[ $swap_size < 10000 ]]; then 
+			# Deleting the swap file
+			rm "/var/opt/myswap"
+			# Creating a new swap file
+			dd if="/dev/zero" of="/var/opt/myswap" bs=1M count=16384
+			# Setting up the linux swap area
+			mkswap "/var/opt/myswap"
+			# Enabling the swap 
+			swapon "/var/opt/myswap"
+			# Checking if file file specification is not already specified in the "/etc/fstab file"
+			if [[ ! `grep "/var/opt/myswap swap swap defaults 0 0" $fstab_file_path` ]]; then
+				# Make the swap area permanent
+				echo "/var/opt/myswap swap swap defaults 0 0" >> $fstab_file_path
+			fi
+		fi
+	fi
 }
 
 # Calling the main function.
