34fa91d698dc962767c994355c7f42a064a284a0
= <=>
2023-03-17 15:54:38 -0400

    Adding comment lines

--- set_up_the_system.sh
+++ set_up_the_system.sh
@@ -26,8 +26,9 @@ main () {
 fix_lines() {
     # A function which fixes lines
 
-    # Looping through file in electro linux's scripts folder and, trantSFileorming the file's lines as the lines without ^M in their ends anymore.
+    # Iterating through file in electro linux's scripts folder and, trantSFileorming the file's lines as the lines without ^M in their ends anymore.
     for script in `/usr/bin/ls $scripts_folder`; do
+        # Removing the invisible characters that are created by windows environment
         sudo sed -i -e 's/\r$//' $scripts_folder"/$script"
     done
 }
@@ -37,6 +38,7 @@ make_executable() {
 
     # Making every single script file in electro linux's scripts folder executable.
     for script in `/usr/bin/ls $scripts_folder`; do
+        # Giving execute permission to user
         sudo chmod u+x $scripts_folder"/$script"
     done
 }
@@ -47,7 +49,7 @@ execute_the_scripts() {
     # Changing the current working directory to the scripts folder.
     cd $scripts_folder
 
-    # Looping through each file in the scripts folder.
+    # Iterating through each file in the scripts folder.
     for script in `/usr/bin/ls $scripts_folder`; do
         # Checking if the file name is not "115_non_su_settings.sh" or "108_installing_yed.sh"
         if [[ $script != "115_non_su_settings.sh" ]] || [[ $script != "108_installing_yed.sh" ]]; then
